a0ff09d874752ce524ab07a2f79df075667ad506
Jennifer Perez <perezjennifer780@gmail.com>
2018-10-22 18:39:38 -0400

    Adding a firebase directory and appending blurb into the body.

--- js/index.js
+++ js/index.js
@@ -4,3 +4,16 @@ import test from './script2.js';
 console.log('test');
 
 test();
+var database = firebase.database();
+
+var databaseRef = database.ref('/');
+
+databaseRef.once('value').then(function(snapshot){
+    const databaseValues = snapshot.val();
+
+$('body').append(databaseValues['blurb']);
+
+
+});
+
+
